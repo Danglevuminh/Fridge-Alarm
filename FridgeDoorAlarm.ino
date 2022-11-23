cd8d42a13c686aa64e7383a9822dcd627ee1cb8e
Dang Le <84720339+Danglevuminh@users.noreply.github.com>
2022-11-22 23:18:45 -0500

    Add files via upload

--- FridgeDoorAlarm.ino
+++ FridgeDoorAlarm.ino
@@ -1,8 +1,9 @@
 
 const int LEDpin = 8;
 const int buzzer = 9; //puzzer to pin 9
+int tempSensorPin = analogRead(A0); 
 void setup() {
-  // put your setup code here, to run once:
+  
   pinMode(LEDpin,OUTPUT);
   pinMode(buzzer, OUTPUT);
   Serial.begin(9600);
@@ -10,22 +11,61 @@ void setup() {
 }
 
 void loop() {
-  int value = analogRead(A0);
-  Serial.println("Analog Value :");
-  Serial.println(value);  
-  delay(500);
+   
+   //Read the analog values of the photoresistor and print it to console for verification
+  int photoValue = analogRead(A0);
+  Serial.println("Analog Value of photoresistor :");
+  Serial.println(photoValue);  
+ 
+ //When the recorded value of the photoresistor goes over 100 - usually room light intensity, the block begins
+
+  if (photoValue>100){
+    
+    delay(30000);
+    photoValue = analogRead(A0);
+
+    //Checks if the analog value of the photoresistor still remains at room light intensity, at which point the buzzer and LED will go off
+    //Lasts until the fridge door is closed
+    while (photoValue>100){
+    tone(buzzer, 1000);
+     digitalWrite(LEDpin, HIGH);
+     delay(100);
+     digitalWrite(LEDpin, LOW);
+     delay(100);
+     photoValue = analogRead(A0);
+    }
+    noTone(buzzer);
+
+
+    
+  }
+   //Looping LED
+  
   
  
-  //Looping LED
-  digitalWrite(LEDpin, HIGH);
-  delay(2000);
-  digitalWrite(LEDpin, LOW);
-  delay(2000);
- /*
+  
+  //Read the analog values of the temperature sensor
+  /*
+  int tempSensorReading = analogRead(A2);
+  Serial.println(tempSensorReading);
+  /*
+  double temp = tempSensorReading/1024;
+  temp = temp*3.3;
+  temp = temp-0.5;
+  temp = temp*100;
+  Serial.println("temperature is ");
+  Serial.println(temp);
+  delay(100);
+  */
+
+
+
+
   //Looping buzzer
-  tone(buzzer, 1000); 
-  delay(1000);        
-  noTone(buzzer);     
-  delay(1000);
-  */  
+  //; 
+      
+ 
+ 
+ 
+    
 }
